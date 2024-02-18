c44fc8401ccf2c55adcd4765fdfeb7be696d1576
Ikeshala <ikeshala.peiris@gmail.com>
2024-02-19 00:07:37 +0530

    Created the yml file for configuration.

--- src/main/java/icet/edu/borrower/controller/BorrowerController.java
+++ src/main/java/icet/edu/borrower/controller/BorrowerController.java
@@ -2,15 +2,18 @@ package icet.edu.borrower.controller;
 
 import icet.edu.borrower.dto.Borrower;
 import icet.edu.borrower.service.BorrowerService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/borrower")
 public class BorrowerController {
+    @Autowired
     BorrowerService service;
-    @PostMapping("/add")
-    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
     public void addBorrower(@RequestBody Borrower borrower){
         service.addBorrower(borrower);
     }
--- src/main/java/icet/edu/borrower/service/impl/BorrowerServiceImpl.java
+++ src/main/java/icet/edu/borrower/service/impl/BorrowerServiceImpl.java
@@ -2,7 +2,9 @@ package icet.edu.borrower.service.impl;
 
 import icet.edu.borrower.dto.Borrower;
 import icet.edu.borrower.service.BorrowerService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BorrowerServiceImpl implements BorrowerService {
     @Override
     public void addBorrower(Borrower borrower) {
